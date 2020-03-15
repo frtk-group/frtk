1e8e87bbf57f975b86d7a83bc6ecf09b087ed720
MazdakShojaie <hrsh@users.noreply.github.com>
2020-03-15 12:11:29 +0330

    Init

---
+++ Consult.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Fartak.Entities.Domain
+{
+    public class Consult
+    {
+        public Consult()
+        {
+            UniqueId = Guid.NewGuid().ToString("D");
+        }
+        [Key] public int Id { get; set; }
+        public string UniqueId { get; set; }
+        public string Name { get; set; }
+        public DateTimeOffset Date { get; set; }
+        public string Mobile { get; set; }
+        public bool IsRead { get; set; }
+    }
+}
