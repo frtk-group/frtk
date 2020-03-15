325aa9678eafda69ed10e601ceb23986bc7b3d07
MazdakShojaie <hrsh@users.noreply.github.com>
2020-03-15 12:18:54 +0330

    Init001

---
+++ Addition.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Fartak.Entities.Domain
+{
+    public class Addition
+    {
+        public Addition()
+        {
+            UniqueId = Guid.NewGuid().ToString("D");
+        }
+        [Key] public int Id { get; set; }
+        public string UniqueId { get; set; }
+        public DateTimeOffset LastModified { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string ImageUrl { get; set; }
+        public double ViewCount { get; set; }
+        public bool IsEnabled { get; set; }
+    }
+}
---
+++ EmailSubscription.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Fartak.Entities.Domain
+{
+    public class EmailSubscription
+    {
+        public EmailSubscription()
+        {
+            UniqueId = Guid.NewGuid().ToString("D");
+        }
+        [Key] public int Id { get; set; }
+        public string UniqueId { get; set; }
+        public DateTimeOffset ContactDate { get; set; }
+        public string Email { get; set; }
+        public bool IsRead { get; set; }
+    }
+}
